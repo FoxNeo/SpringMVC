67347012ee2f0fc54e5a76e7ec0250ed03537aa6
Caceres Miguel <miguelcaceres94@hotmail.com>
2017-10-08 17:44:21 +0200

    Initial Commit SpringMVC project configs

---
+++ src/main/java/de/caceres/spring/web/controller/IndexController.java
@@ -0,0 +1,13 @@
+package de.caceres.spring.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping(value = "/")
+	public String home() {
+		return "index";
+	}
+}
---
+++ src/main/java/de/caceres/web/spring/config/RootConfig.java
@@ -0,0 +1,14 @@
+package de.caceres.web.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 
+ * @author miguel caceres
+ * 
+ *
+ */
+@Configuration
+public class RootConfig {
+
+}
